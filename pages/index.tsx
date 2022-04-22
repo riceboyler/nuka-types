1c4fa6e0059f0d3eb989d36c75dde74e558264ac
Jason Clark <riceboyler@gmail.com>
2022-04-22 15:59:22 -0600

    Add nuka-carousel and show errors

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Carousel from "nuka-carousel";
+
+
+export default function Home() {
+  return (
+    <Carousel>
+      <Image src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" />
+      <Image src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" />
+    </Carousel>
+  );
+}
